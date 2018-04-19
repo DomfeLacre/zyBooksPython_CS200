4f3a9871f2daa4f061b4c93d9ad4bc11eb13ded8
thewickk <thewickk@thewickk.com>
2018-04-19 15:23:29 -0400

    4/19 end of workday commit

--- module6/7_15_ReeBOOOT.py
+++ module6/7_15_ReeBOOOT.py
@@ -1,11 +1,11 @@
 movieCollection = {
     2005: ['Munich', 'Steven Spielberg'],
-    2006: (['The Prestige', 'Christopher Nolan'], ['The Departed', 'Martin Scorsese']),
+    2006: [['The Prestige', 'Christopher Nolan'], ['The Departed', 'Martin Scorsese']],
     2007: ['Into the Wild', 'Sean Penn'],
     2008: ['The Dark Knight', 'Christopher Nolan'],
     2009: ['Mary and Max', 'Adam Elliot'],
     2010: ['The King\'s Speech', 'Tom Hooper'],
-    2011: (['The Artist', 'Michel Hazanavicius'], ['The Help', 'Tate Taylor']),
+    2011: [['The Artist', 'Michel Hazanavicius'], ['The Help', 'Tate Taylor']],
     2012: ['Argo', 'Ben Affleck'],
     2013: ['12 Years a Slave', 'Steve McQueen'],
     2014: ['Birdman', 'Alejandro G. Inarritu'],
@@ -18,7 +18,20 @@ movieKeyList = list(movieCollection.keys())
 movieValueList = list(movieCollection.values())
 movieItemsList = list(movieCollection.items())
 
-for year in movieKeyList:
-    print(movieCollection.get(year))
+
+# for key, value in movieItemsList:
+#     if key == userInput:
+#         newList = []
 
 
+
+# for key, value in enumerate(movieItemsList):
+#     if value[0] == userInput:
+
+# strItem = str(testItem)
+# strItem = strItem.strip('[]')
+# print(strItem)
+# for i in movieItemsList:
+#     for j in i:
+#         if type(j) == list:
+#             print(j)
